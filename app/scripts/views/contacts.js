c629ce768e0b24c0854b0d68aacb4bec4be213c8
Oeromilk <oero.mccomb@gmail.com>
2016-10-19 22:53:42 -0400

    working modals

--- app/scripts/views/contacts.js
+++ app/scripts/views/contacts.js
@@ -1,8 +1,8 @@
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass/assets/javascripts/bootstrap');
 var Backbone = require('backbone');
-var contactFormTemplate = require('../templates/contactforms.hbs');
-var contactTableTemplate = require('../templates/contacttable.hbs');
+var contactFormTemplate = require('../../templates/contactforms.hbs');
+var contactTableTemplate = require('../../templates/contacttable.hbs');
 
 var ContactInstructionView = Backbone.View.extend({
   tagName: 'h3',
@@ -59,10 +59,12 @@ var ContactsTableView = Backbone.View.extend({
     'class': 'table table-bordered'
   },
   initialize: function(){
-    this.listenTo(this.collection, 'add', this.renderContactTable)
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.collection, 'add', this.renderContactTable);
     this.$el.append(
       '<thead>'+ '<tr>' + '<th>' + 'Email' + '</th>' + '<th>' + 'Name' + '</th>'
-      + '<th>' + 'Phone Number' + '</th>' + '<th>' + 'Twitter' + '</th>');
+      + '<th>' + 'Phone Number' + '</th>' + '<th>' + 'Twitter' + '</th>' + '<th>'
+      + 'Actions' + '</th>' + '</tr>' + '</thead>');
   },
   renderContactTable: function(contact){
     var contactRow = new ContactRowView({model: contact});
@@ -75,14 +77,86 @@ var ContactsTableView = Backbone.View.extend({
 var ContactRowView = Backbone.View.extend({
   tagName: 'tbody',
   template: contactTableTemplate,
+  events: {
+    'click #remove': 'remove',
+    'click #update': 'update'
+  },
+  initialize: function(){
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'changed', this.render);
+  },
   render: function(){
     var context = this.model.toJSON();
     var rowTemp = this.template(context);
     this.$el.html(rowTemp);
 
     return this;
+  },
+  remove: function(){
+    confirmModal.model = this.model;
+
+    confirmModal.show();
+  },
+  update: function(){
+    submissionModal.model = this.model;
+
+    submissionModal.show();
   }
 });
+
+var ContactConfirmModal = Backbone.View.extend({
+  el: $('#confirm-modal')[0],
+  events: {
+    'click #delete-contact': 'delete'
+  },
+  hide: function(){
+    this.$el.modal('hide');
+  },
+  show: function(){
+    this.$el.modal('show');
+  },
+  delete: function(){
+    this.model.destroy();
+
+    this.hide();
+  }
+});
+
+var confirmModal = new ContactConfirmModal();
+
+var ContactSubmissionModal = Backbone.View.extend({
+  el: $('#resubmit-modal')[0],
+  events: {
+    'submit .addContact': 'addContact'
+  },
+  hide: function(){
+    this.$el.modal('hide');
+  },
+  show: function(){
+    this.$el.modal('show');
+  },
+  addContact: function(event){
+    event.preventDefault();
+
+    var contactEmail = $('#email').val();
+    var contactName = $('#name').val();
+    var contactNumber = $('#phone-number').val();
+    var twitterName = $('#twitter-username').val();
+    this.collection.create({
+      email: contactEmail,
+      name: contactName,
+      'phone-number': contactNumber,
+      twitter: twitterName
+    });
+
+    $('#email').val('');
+    $('#name').val('');
+    $('#phone-number').val('');
+    $('#twitter-username').val('');
+  }
+});
+
+var submissionModal = new ContactSubmissionModal();
 
 module.exports = {
   ContactInstructionView: ContactInstructionView,
